960d55f96bde335ea5b556bfde73c64852321bb8
Ronnie <148753940+Nessumsa@users.noreply.github.com>
2023-10-24 09:26:58 +0200

    Added substractMethod

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,4 +2,10 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+public static int substractMethod (int num1, int num2){
+        return num1 - num2;
+}
+
+
 }
