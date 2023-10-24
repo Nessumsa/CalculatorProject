9f6edf06bb4f94231e0c262393ba18ae073d60c9
ELIAS THERKILDSEN <73726902+eliastherkildsen@users.noreply.github.com>
2023-10-24 09:28:24 +0200

    add add method

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,4 +2,10 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static int add(int n1, int n2){
+        return n1 + n2;
+
+    }
+
 }
