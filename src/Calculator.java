74fc2c24d34d04498c40a4ff0725a6b876e35d61
Ronnie <148753940+Nessumsa@users.noreply.github.com>
2023-10-24 09:44:54 +0200

    Added comment in end

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,17 +6,11 @@ public class Calculator {
     public static int substractMethod (int num1, int num2){
             return num1 - num2;
     }
-
-
-
     public static int add(int n1, int n2){
         return n1 + n2;
 
     }
-
     public static int tripleAdd(int num1, int num2, int num3){
         return add(add(num1, num2),num3);
     }
-
-
-}
+} //END OF CALCULATOR CLASS
